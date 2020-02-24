e232a14978eb2d59b4ad7be2a10156d2c611e58b
81254807 <siguajener@yahoo.com>
2020-02-24 10:53:52 +0800

    remove service on publis apis

--- projects/formly-builder/src/public-api.ts
+++ projects/formly-builder/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of formly-builder
  */
 
-export * from './lib/formly-builder.service';
+// export * from './lib/formly-builder.service';
 export * from './lib/formly-builder.component';
 export * from './lib/formly-builder.module';
