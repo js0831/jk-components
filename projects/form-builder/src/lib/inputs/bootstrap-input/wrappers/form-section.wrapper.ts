51005291233cc48547d3b20072e67fc2efa27346
81254807 <siguajener@yahoo.com>
2020-03-03 09:16:27 +0800

    add edit on wrapper

--- projects/form-builder/src/lib/inputs/bootstrap-input/wrappers/form-section.wrapper.ts
+++ projects/form-builder/src/lib/inputs/bootstrap-input/wrappers/form-section.wrapper.ts
@@ -7,6 +7,10 @@ template: `
   <div class="card">
     <div class="card-header">
       {{to.label}}
+
+      <jk-edit-input-button
+        [field]="field" [template]="to"
+      ></jk-edit-input-button>
     </div>
     <div class="card-body">
       <ng-container #fieldComponent></ng-container>
@@ -16,8 +20,21 @@ template: `
 styles: [
   `
     :host{
+      position:relative;
       display: block;
       margin-bottom: 10px;
+    }
+
+    .card:hover {
+      box-shadow: 0 0 0px 3px rgba(255, 131, 0, 0.5);
+    }
+
+    .card-header:hover jk-edit-input-button{
+      display:block;
+    }
+
+    jk-edit-input-button{
+      display:none;
     }
   `
 ]
