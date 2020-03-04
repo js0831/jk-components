d7438412d698630874d075efc13cde31d870697f
81254807 <siguajener@yahoo.com>
2020-03-04 16:25:45 +0800

    blank option on select

--- projects/form-builder/src/lib/components/editor/components/editor-options-tab/editor-options-tab.component.ts
+++ projects/form-builder/src/lib/components/editor/components/editor-options-tab/editor-options-tab.component.ts
@@ -11,17 +11,18 @@ export class EditorOptionsTabComponent implements OnInit {
   @Input() form: FormArray;
   id = '';
   label = '';
+  fieldType: string;
 
   constructor(
     private fb: FormBuilder
   ) { }
 
   ngOnInit() {
-    //
+    this.fieldType = this.form.parent.value.main.type;
   }
 
   add() {
-    if (this.id.length > 0 && this.label.length > 0) {
+    if ((this.id.length > 0 && this.label.length > 0) || this.fieldType === 'select') {
       const group = new FormGroup({
         id: new FormControl(this.id),
         label: new FormControl(this.label)
